57cc67bcf23d9e49ca0ec3493f1a05e1c0fdb26d
mauriani <maurianim@gmail.com>
2022-09-15 13:59:51 -0300

    first commit

---
+++ server/src/utils/convert-minutes-string-to-hour.ts
@@ -0,0 +1,12 @@
+// 1080 -> 18:00
+
+export function convertMinutesStringToHour(minutesAmount: number) {
+  // 18:00 -> ["18", "00"] -> [18,00]
+  const hours = Math.floor(minutesAmount / 60);
+  const minutes = minutesAmount % 60;
+
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+    2,
+    "0"
+  )}`;
+}
