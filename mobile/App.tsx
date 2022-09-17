bbd0fdb2ab0c6c65c139af4a7d7dec7d17ca6224
Mauriani Maciel <maurianim@gmail.com>
2022-09-17 20:05:31 -0300

    5° aula

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -6,18 +7,62 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
+import * as Notifications from "expo-notifications";
+
 import { Background } from "./src/components/Background";
 
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
 
+import "./src/services/notificationConfig";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+
+  // quando vc responde a notificacao e vai para o app
+  const responseNotificationListener = useRef<Subscription>();
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_900Black,
   });
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    // observa quando recebo uma notificacao
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    // observar quando respondo
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((respose) => {
+        console.log(respose);
+      });
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
+
   return (
     <Background>
       <StatusBar
